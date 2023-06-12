e134899cb8b3a8b93d12b6e29a40951d6ba3f253
RidhoRPL <543211174@smktelkom-bjb.sch.id>
2023-06-12 08:53:50 +0800

    tambahan

--- src/data/data.js
+++ src/data/data.js
@@ -32,32 +32,32 @@ export const data = [
         name: 'Donut Coklat',
         category: 'donut',
         image:
-            'https://images.unsplash.com/photo-1631143070457-c1aecc92abbc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80donut',
+            'https://images.unsplash.com/photo-1626094309830-abbb0c99da4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80',
     },
     {
         id: 6,
         name: 'Donut Coklat',
         category: 'donut',
         image:
-            'https://images.unsplash.com/photo-1631143070457-c1aecc92abbc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80donut',
+            'https://plus.unsplash.com/premium_photo-1679339214151-11948ee56ae2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
     },
     {
         id: 7,
         name: 'Donut Coklat',
         category: 'donut',
         image:
-            'https://images.unsplash.com/photo-1631143070457-c1aecc92abbc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80donut',
+            'https://images.unsplash.com/photo-1624277904878-120554c46c2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80',
     },
     {
         id: 8,
         name: 'Donut Coklat',
         category: 'donut',
         image:
-          'https://images.unsplash.com/photo-1631143070457-c1aecc92abbc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80donut',
+          'https://images.unsplash.com/photo-1558005288-70a04248aca9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=835&q=80',
     },
     {
         id: 9,
-        name: 'Donut Coklat',
+        name: 'Minuman',
         category: 'drink',
         image:
           'https://images.unsplash.com/photo-1609951651556-5334e2706168?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
@@ -65,9 +65,65 @@ export const data = [
     },
     {
         id: 10,
-        name: 'Donut Coklat',
+        name: 'Minuman',
         category: 'drink',
         image:
           'https://images.unsplash.com/photo-1546171753-97d7676e4602?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+    },
+    {
+        id: 11,
+        name: 'Minuman',
+        category: 'drink',
+        image:
+          'https://images.unsplash.com/photo-1592858167090-2473780d894d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+    },
+    {
+        id: 12,
+        name: 'Minuman',
+        category: 'drink',
+        image:
+          'https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=957&q=80',
+    },
+    {
+        id: 13,
+        name: 'Minuman',
+        category: 'drink',
+        image:
+          'https://images.unsplash.com/photo-1556679343-c7306c1976bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80',
+    },
+    {
+        id: 14,
+        name: 'Minuman',
+        category: 'drink',
+        image:
+          'https://images.unsplash.com/photo-1534353473418-4cfa6c56fd38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+    },
+    {
+        id: 15,
+        name: 'Minuman',
+        category: 'drink',
+        image:
+          'https://images.unsplash.com/photo-1551538827-9c037cb4f32a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1530&q=80',
+    },
+    {
+        id: 16,
+        name: 'Minuman',
+        category: 'drink',
+        image:
+          'https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+    },
+    {
+        id: 17,
+        name: 'Yg lain',
+        category: 'other',
+        image:
+          'https://images.unsplash.com/photo-1534567110243-8875d64ca8ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+    },
+    {
+        id: 18,
+        name: 'Yg lain',
+        category: 'other',
+        image:
+          'https://images.unsplash.com/photo-1538127426967-75a6c73f6d20?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80',
     },
 ];
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import { data } from "../data/data"
 import 'swiper/css';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
 import ScrollToTop from 'react-scroll-to-top';
 import '../menu.css';
 import Bleehhh from '../components/navbar';
 import Footer from '../components/footer';
 
 const Menu = () => {
+  Aos.init({
+    duration: 1800,
+    offset: 3,
+  })
   const [donut, setDonut] = useState(data)
 
   const filterType =(category) => {
@@ -21,23 +27,23 @@ const Menu = () => {
 
     <div>
       <Bleehhh />
-      <div pt-4>
+      <div pt-4 data-aos="zoom-in">
         <h1 className='text-center text-[26px] pt-10 font-bold'>MENU LIST</h1>
         <p className='text-center underline decoration-2 decoration-pink-600  font-bold text-white'>UNDERLINE UNDERLINE UNDERLINE</p>
       </div>
 
-      <div className='pt-5 pl-72 flex-col'>
+      <div className='pt-5 pl-72 flex-col' data-aos="zoom-in">
         <div className='flex gap-8 ml-60'>
-          <button onClick={() => setDonut(data)} className='text-amber-600 pl-2'>All Menu</button>
-          <button onClick={() => filterType('donut')} href='' className='text-amber-600'>Donut</button>
-          <button onClick={() => filterType('drink')} href='' className='text-amber-600'>Drink</button>
-          <button className='text-amber-600'>Other</button>
+          <button onClick={() => setDonut(data)} className='text-amber-600 pl-2 hover:scale-110 duration-300'>All Menu</button>
+          <button onClick={() => filterType('donut')} className='text-amber-600 hover:scale-110 duration-300'>Donut</button>
+          <button onClick={() => filterType('drink')} className='text-amber-600 hover:scale-110 duration-300'>Drink</button>
+          <button onClick={() => filterType('other')} className='text-amber-600 hover:scale-110 duration-300'>Other</button>
         </div>
       </div>
 
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-12 p-14" data-aos="fade-up">
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-12 p-14">
         {donut.map((item, index) => (
-          <div key={index} className="border shadow-lg hover:scale-105 duration-300">
+          <div key={index} className="border shadow-lg hover:scale-105 duration-300" data-aos="fade-up">
             <img className="w-full h-[200px] object-cover rounded-t-lg" src={item.image} alt={item.name} />
             <div className="flex justify-between px-2 py-4 ">
               <p className="font-bold">{item.name}</p>
